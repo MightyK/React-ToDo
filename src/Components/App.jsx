f750710c07e4cc2047ab6c19a9e12a30229d86ed
MightyK <KMightyDev@gmail.com>
2021-03-11 21:15:20 -0500

    Final app setup complete

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+
+    const [inputText, setInputText] = useState('');
+    const [items, setItems] = useState([]);
+
+    const handleChange = (event) => {
+        const newValue = event.target.value;
+        
+        setInputText(newValue);
+    }
+
+    const addItem = () => {
+        setItems( (prevItems) => {
+            return [
+                ...prevItems,
+                inputText
+            ];
+        });
+        setInputText('');
+    }
+
     return (
         <div className='container'>
             <div className='heading'>
                 <h1>To-Do List</h1>
             </div>
             <div className='form'>
-                <input type='text' />
-                <button>
+                <input 
+                    type='text' 
+                    onChange={handleChange}
+                    value={inputText} 
+                />
+                <button
+                    onClick={addItem}
+                >
                     <span>Add</span>
                 </button>
             </div>
             <div>
                 <ul>
-                    <li>A Item</li>
+                    {items.map( (toDoItem) => {
+                        return (
+                            <li>{toDoItem}</li>
+                        )
+                    })}
                 </ul>
             </div>
         </div>
